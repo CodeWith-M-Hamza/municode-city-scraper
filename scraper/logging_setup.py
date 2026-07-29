4a59181adce189017f660801f3b89a9ee02be837
CodeWith-M-Hamza <hamzasattar5003@gmail.com>
2026-07-29 18:13:10 +0500

    feat: add driver setup and logging module

---
+++ scraper/logging_setup.py
@@ -0,0 +1,20 @@
+import logging
+import os 
+def get_logger(city_name):
+    logger=logging.getLogger(city_name)
+    logger.setlevel(logging.DEBUG)
+    if not logger.handlers:
+        log_folder="logs"
+        os.makedirs(log_folder,exist_ok=True)
+        log_path=os.path.join(log_folder,f"{city_name}")
+        file_handler=logging.FileHandler(log_path)
+        file_handler.setLevel(logging.INFO)
+        console_handler=logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+        formatter=logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+    return logger
+        
---
+++ scraper/municode/driver_setup.py
@@ -0,0 +1,15 @@
+from selenium import webdriver 
+from selenium.webdriver.chrome.options import Options
+
+def get_driver(headless: bool = False) -> webdriver.Chrome:
+    options=Options()
+    if headless:
+        options.add_argument('--headless=new')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+    driver=webdriver.Chrome(options=options)
+    driver.implicitly_wait(0)
+    return driver 
+    
